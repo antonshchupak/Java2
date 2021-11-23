d8d2d317d3212cee0db549d87272bad488b2b8cf
antonshchupak <90201722+antonshchupak@users.noreply.github.com>
2021-11-23 13:21:25 +0300

    Uploaded lesson5 homework file Main

--- Main.java
+++ Main.java
@@ -1,30 +1,63 @@
-package lesson3.task2;
+import java.util.Arrays;
 
-public class Main  {
-    public static void main(String[] args) {
-        PhoneBook phoneBook = new PhoneBook();
-        phoneBook.add("Jacobs", "89001001010");
-        phoneBook.add("Peters", "891001001011");
-        phoneBook.add("Peters", "891001001012");
-        phoneBook.add("Williams", "891001002020");
-        phoneBook.add("Brown", "890001001011");
-        phoneBook.add("Brown", "891001054011");
+public class Main {
+    static final int size = 10000000;
+    static final int h = size / 2;
 
-        System.out.println("Jacobs");
-        for (String number: phoneBook.get("Jacobs")) {
-            System.out.println(" " + number);
-        }
-        System.out.println("Peters");
-        for (String number: phoneBook.get("Peters")) {
-            System.out.println(" " + number);
-        }
-        System.out.println("Williams");
-        for (String number: phoneBook.get("Williams")) {
-            System.out.println(" " + number);
-        }
-        System.out.println("Brown");
-        for (String number: phoneBook.get("Brown")) {
-            System.out.println(" " + number);
-        }
+    public static float[] firstMethod() {
+        float[] arr = new float[size];
+        Arrays.fill(arr, 1.0f);
+        long startTime  = System.currentTimeMillis();
+        for(int i = 0; i < arr.length; i ++) {
+                arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        System.out.println("One thread time is: " + (System.currentTimeMillis() - startTime) + " ms.");
+        return arr;
+    }
+
+    public static float[] secondMethod() throws InterruptedException {
+        float[] arr = new float[size];
+        Arrays.fill(arr, 1.0f);
+        long startTime  = System.currentTimeMillis();
+        float[] a1 = new float[h];
+        float[] a2 = new float[h];
+        System.arraycopy(arr,0,a1,0,h);
+        System.arraycopy(arr,h,a2,0,h);
+        long cutTime  = System.currentTimeMillis();
+        System.out.println("Cutting time is: " + (cutTime - startTime) + " ms.");
+        Thread t1 = new Thread(() -> {
+            long fillTimeOne  = System.currentTimeMillis();
+            for(int i = 0; i < a1.length; i ++) {
+                a1[i] = (float)(a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+            long fillTimeOneFinish = System.currentTimeMillis();
+            System.out.println("Filling first part of array time is: " + (fillTimeOneFinish - fillTimeOne) + " ms.");
+        });
+        Thread t2 = new Thread(() -> {
+            long fillTimeTwo  = System.currentTimeMillis();
+            for(int i = 0; i < a2.length; i ++) {
+                int j = i + h;
+                a2[i] = (float)(a2[i] * Math.sin(0.2f + j / 5) * Math.cos(0.2f + j / 5) * Math.cos(0.4f + j / 2));
+            }
+            long fillTimeTwoFinish = System.currentTimeMillis();
+            System.out.println("Filling second part of array time is: " + (fillTimeTwoFinish - fillTimeTwo) + " ms.");
+        });
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        long arrAgainStart = System.currentTimeMillis();
+        float[] arrFilledTogether = new float[size];
+        System.arraycopy(a1, 0, arrFilledTogether, 0, h);
+        System.arraycopy(a2, 0, arrFilledTogether, h, h);
+        long allTime = System.currentTimeMillis();
+        System.out.println("Thread compile back to arrFilledTogether time is: " + (allTime - arrAgainStart) + " ms.");
+        System.out.println("Execution all process time is: " + (allTime - startTime) + " ms.");
+        return arrFilledTogether;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        firstMethod();
+        secondMethod();
     }
 }
